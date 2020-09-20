a9b96142c590ed109cd600b2543fd8c7ea460ee1
Charles Hapner <chapner91@gmail.com>
2020-09-20 15:52:09 -0400

    Rearranged project folder heirarchy

--- IntegrationManager/IntegrationManager.Database/Tables/DatasourceElement.sql
+++ IntegrationManager/IntegrationManager.Database/Tables/DatasourceElement.sql
@@ -1,8 +1,9 @@
 ﻿CREATE TABLE [LKP].[DatasourceElement]
 (
 	[DatasourceElementID] INT NOT NULL IDENTITY, 
-    [DatasourceElementName] VARCHAR(50) NULL, 
     [DatasourceID] INT NOT NULL, 
+    [DatasourceElementName] VARCHAR(50) NULL, 
+    [DatasourceElementDefinition] VARCHAR(MAX) NULL, 
     CONSTRAINT [FK_DatasourceElement_Datasource] FOREIGN KEY ([DatasourceID]) REFERENCES [LKP].[Datasource]([DatasourceID]), 
     CONSTRAINT [PK_DatasourceElement] PRIMARY KEY ([DatasourceElementID])
 )
