505ba74f96f8bba7a1b9f4ed3eb52c7a2fb57611
Charles Hapner <chapner91@gmail.com>
2020-09-27 19:04:28 -0400

    Add Extensions class library

---
+++ IntegrationManager/Extentions/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extentions
+{
+    public static class StringExtensions
+    {
+		public static string[] Split(this string @this, char separator, int count)
+		{
+			if (@this == null)
+			{
+				throw new ArgumentNullException(nameof(@this));
+			}
+			return @this.Split(new[] { separator }, count);
+
+		}
+    }
+}
